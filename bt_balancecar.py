7f1abf11fea637814524956cfd29865b9e8bd8a6
LeBronLiHD <lebronlihd@outlook.com>
2021-12-28 20:53:47 +0800

    bluetooth done!

--- bt_balancecar.py
+++ bt_balancecar.py
@@ -15,7 +15,8 @@ import math
 import numpy
 
 server_address = "98:DA:20:02:F7:57"
-server_port = 4  # default
+server_port = 1  # default
+ultra_distance = -1
 
 try:
     socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -25,19 +26,44 @@ except OSError as e:
     print(f"{server_address} is offline", e)
     exit()
 
-mode_dict = {'w':'$1,0,0,0,0,0,0,0,0,0#',
-             'a':'$3,0,0,0,0,0,0,0,0,0#',
-             's':'$2,0,0,0,0,0,0,0,0,0#',
-             'd':'$4,0,0,0,0,0,0,0,0,0#',
-             'q':'$0,0,0,0,0,0,0,0,0,0#'}
+print("bluetooth connected!")
+mode_dict = {'w': '$1,0,0,0,0,0,0,0,0,0#',
+             'a': '$3,0,0,0,0,0,0,0,0,0#',
+             's': '$2,0,0,0,0,0,0,0,0,0#',
+             'd': '$4,0,0,0,0,0,0,0,0,0#',
+             'q': '$0,0,0,0,0,0,0,0,0,0#'}
+
+
+def quit_connection():
+    print("bluetooth closed.")
+    socket.close()
+
+
+def read_ultra():
+    return ultra_distance
+
+
+def refresh_ultra(data):
+    de_data = data.decode('utf-8')  # bytes -> str
+    print("recv <" + de_data + "> from device:" + server_address)
+    idx_1, idx_2 = 0, 0
+    for i in range(len(de_data)):
+        char = de_data[i]
+        if char == ',':
+            idx_1 = i
+        if char == 'c' and i < len(de_data) - 1 and de_data[i + 1] == 'm':
+            idx_2 = i
+            break
+    str_ultra = de_data[idx_1 + 1:idx_2]
+    ultra_distance = float(str_ultra)
+    print("ultra distance ->", ultra_distance)
 
 
 def control(mode=None):
     text = mode_dict[mode]
     socket.send(text)
-    if mode == 'q':
-        print("socket closd")
-        socket.close()
+    data = socket.recv(1024)
+    refresh_ultra(data)
 
 
 def run_test():
@@ -46,10 +72,11 @@ def run_test():
     control(mode='s')
     time.sleep(0.5)
     control(mode='a')
-    time.sleep(0.5)
+    time.sleep(2)
     control(mode='d')
-    time.sleep(0.5)
+    time.sleep(2)
     control(mode='q')
+    quit_connection()
 
 
 if __name__ == "__main__":
--- bt_client.py
+++ bt_client.py
@@ -9,6 +9,11 @@ import bluetooth
 
 serverMACAddress = "98:DA:20:02:F7:57"
 port = 1
+mode_dict = {'w': '$1,0,0,0,0,0,0,0,0,0#',
+             'a': '$3,0,0,0,0,0,0,0,0,0#',
+             's': '$2,0,0,0,0,0,0,0,0,0#',
+             'd': '$4,0,0,0,0,0,0,0,0,0#',
+             'q': '$0,0,0,0,0,0,0,0,0,0#'}
 try:
     socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     # HCI, L2CAP, RFCOMM, or SCO. The default is RFCOMM.
@@ -17,13 +22,30 @@ except OSError as e:
     print(f"{serverMACAddress} is offline", e)
     exit()
 
+print("bluetooth connected!")
+
 while 1:
     text = input()  # Note change to the old (Python 2) raw_input
+    text = mode_dict[text]
     if text == '$0,0,0,0,0,0,0,0,0,0#':
         print("sent <" + text + "> to device:" + serverMACAddress)
         socket.send(text)
-    	break
+        break
     print("sent <" + text + "> to device:" + serverMACAddress)
     socket.send(text)
+    data = socket.recv(1024)
+    de_data = data.decode('utf-8')  # bytes -> str
+    print("recv <" + de_data + "> from device:" + serverMACAddress)
+    idx_1, idx_2 = 0, 0
+    for i in range(len(de_data)):
+        char = de_data[i]
+        if char == ',':
+            idx_1 = i
+        if char == 'c' and i < len(de_data) - 1 and de_data[i + 1] == 'm':
+            idx_2 = i
+            break
+    str_ultra = de_data[idx_1 + 1:idx_2]
+    ultra = float(str_ultra)
+    print("ultra distance ->", ultra)
 
 socket.close()
